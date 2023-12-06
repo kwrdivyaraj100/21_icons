01b95d25b4df196dac8e9c055d0ac07463861441
Divya R <kwrdivyaraj100@gmail.com>
2023-12-06 16:00:23 +0530

    Created Test component with AppBar and IconButton

--- mui-tutorial/src/Test.jsx
+++ mui-tutorial/src/Test.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import DownloadForOfflineIcon from '@mui/icons-material/DownloadForOffline';
+import { AppBar, Box, IconButton, Toolbar } from '@mui/material';
 
 function Test() {
     return (
-        
+        <Box>
+            <AppBar>
+                <Toolbar>
+                    <IconButton onClick={() => alert("Icon Selected")}>
+                        {" "}
+                        <AccountCircleIcon />
+                    </IconButton>
+                    <IconButton>
+                        {" "}
+                        <DownloadForOfflineIcon color='inherit' />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+        </Box>
+
     )
 }
 
